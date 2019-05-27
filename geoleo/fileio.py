99b6666fb041a91e2bd0237d71d0817976f123a0
SephirotJustin <justindroegemeier@outlook.de>
2019-05-27 20:31:06 +0000

    added fileio module, methods doesnt work

---
+++ geoleo/fileio.py
@@ -0,0 +1,12 @@
+class GeoLeo:
+    pointcloud = None
+    cadaster = None
+
+    def read_cadaster(self, path):
+        return None
+    
+    def read_pointcloud(self, path):
+        return None
+ 
+    def write_pointcloud(self, pointCloudList):
+        return None 
