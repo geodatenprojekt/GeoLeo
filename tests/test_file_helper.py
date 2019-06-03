a537c6843060fc1e03fb22995d37746210a2c7a0
valentin235 <valentin.hertel@fh-bielefeld.de>
2019-06-03 11:37:48 +0200

    test get_all_paths_from_dir method from file_helper.py

---
+++ tests/test_file_helper.py
@@ -0,0 +1,28 @@
+import unittest
+import shutil
+from geoleo import file_helper
+import os
+
+class Test_file_helper(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+
+    def test_file_helper_get_all_paths_from_dir_ReadAllFiles(self):
+        os.mkdir("test")
+        file1 = open("test/test1", "w")
+        file2 = open("test/test2", "w")
+        file1.close();
+        file2.close();
+        test_path_list = []
+        test_path_list.append("test\\test1")
+        test_path_list.append("test\\test2")
+        path_list = file_helper.get_all_paths_from_dir("test")
+        shutil.rmtree("./test")
+        self.assertEqual(test_path_list, path_list)
+
+    def tearDown(self) -> None:
+        pass
+
