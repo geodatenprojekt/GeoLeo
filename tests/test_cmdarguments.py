727290bf2f31f2cb9e87cd15c325020bb86642bd
valentin235 <valentin.hertel@fh-bielefeld.de>
2019-06-04 11:59:10 +0200

    create the long arg tests

--- tests/test_cmdarguments.py
+++ tests/test_cmdarguments.py
@@ -5,21 +5,23 @@ class Test_cmdarguments(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_Parameters_PointCloudPath(self):
+    def test_Parameters_PointCloudPath_short(self):
         sys.argv.append("-p")
         sys.argv.append("example_data")
         para = cmdargs.Parameters()
 
         self.assertEqual(para.getPointCloudPath(),
                          "example_data")
-    """
-    def test_Parameters_PointCloudNotAPath(self):
-        sys.argv.append("-p")
-        sys.argv.append("NoPath")
-        with self.assertRaises(argparse.ArgumentTypeError):
-            para = cmdargs.Parameters()
-    """
-    def test_Parameters_CadasterPath(self):
+
+    def test_Parameters_PointCloudPath_long(self):
+        sys.argv.append("--pointcloud")
+        sys.argv.append("example_data")
+        para = cmdargs.Parameters()
+
+        self.assertEqual(para.getPointCloudPath(),
+                         "example_data")
+
+    def test_Parameters_CadasterPath_short(self):
         sys.argv.append("-c")
         sys.argv.append("example_data")
         para = cmdargs.Parameters()
@@ -27,7 +29,15 @@ class Test_cmdarguments(unittest.TestCase):
         self.assertEqual("example_data",
                          para.getCadasterPath())
 
-    def test_Parameters_Output(self):
+    def test_Parameters_CadasterPath_long(self):
+        sys.argv.append("--cadaster")
+        sys.argv.append("example_data")
+        para = cmdargs.Parameters()
+
+        self.assertEqual("example_data",
+                         para.getCadasterPath())
+
+    def test_Parameters_Output_short(self):
         sys.argv.append("-o")
         sys.argv.append("example_data")
         para = cmdargs.Parameters()
@@ -35,7 +45,15 @@ class Test_cmdarguments(unittest.TestCase):
         self.assertEqual("example_data",
                          para.getOutputPath())
 
-    def test_Parameters_XOffset(self):
+    def test_Parameters_Output_long(self):
+        sys.argv.append("--output")
+        sys.argv.append("example_data")
+        para = cmdargs.Parameters()
+
+        self.assertEqual("example_data",
+                         para.getOutputPath())
+
+    def test_Parameters_XOffset_short(self):
         sys.argv.append("-x")
         sys.argv.append("50")
         para = cmdargs.Parameters()
@@ -43,8 +61,24 @@ class Test_cmdarguments(unittest.TestCase):
         self.assertEqual(50.0,
                          para.getXOffset())
 
-    def test_Parameters_YOffset(self):
+    def test_Parameters_XOffset_long(self):
+        sys.argv.append("--xoffset")
+        sys.argv.append("50")
+        para = cmdargs.Parameters()
+
+        self.assertEqual(50.0,
+                         para.getXOffset())
+
+    def test_Parameters_YOffset_short(self):
         sys.argv.append("-y")
+        sys.argv.append("50")
+        para = cmdargs.Parameters()
+
+        self.assertEqual(50.0,
+                         para.getYOffset())
+
+    def test_Parameters_YOffset_long(self):
+        sys.argv.append("--yoffset")
         sys.argv.append("50")
         para = cmdargs.Parameters()
 
