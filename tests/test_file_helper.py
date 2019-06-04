e7db139836296b85e112f9f73d6c60f7ccd9edf0
valentin235 <valentin.hertel@fh-bielefeld.de>
2019-06-04 16:10:28 +0200

    Add none path test and comments

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -9,8 +9,13 @@ class Test_file_helper(unittest.TestCase):
         pass
 
 
+    def test_file_helper_get_all_paths_from_dir_none__path(self):
+        """Test the method get all paths from dir withe None as parameter.
+            It should return None."""
+        self.assertEqual(None, file_helper.get_all_paths_from_dir(None))
 
     def test_file_helper_get_all_paths_from_dir_ReadAllFiles(self):
+        """"Test the method get all paths from dir withe a valid direction and two files in it """
         os.mkdir("test")
         file1 = open("test/test1", "w")
         file2 = open("test/test2", "w")
@@ -23,7 +28,9 @@ class Test_file_helper(unittest.TestCase):
         shutil.rmtree("./test")
         self.assertEqual(test_path_list, path_list)
 
-    def test_file_helper_get_all_paths_from_dir_NoPath(self):
+    def test_file_helper_get_all_paths_from_dir_not_valid_path(self):
+        """Test the method get all paths from dir withe an not existing direction.
+            It should raise an file not found exception"""
         with self.assertRaises(FileNotFoundError):
             file_helper.get_all_paths_from_dir("test")
 
