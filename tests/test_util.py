9d8145a6d35ec60261220044f82a9a4225cf3d23
valentin235 <valentin.hertel@fh-bielefeld.de>
2019-06-11 15:13:31 +0200

    add unzip LAZFILE test case

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import os
 from geoleo import util
 
 
@@ -29,7 +30,7 @@ class TestUtil(unittest.TestCase):
         print(string)"""
         self.assertTrue(True)
 
-    def test_util_get_points_close_to_anchor_same_YAches(self):
+    def test_get_points_close_to_anchor_same_YAches(self):
         """Test the method "get Points close to anchor" white Points on the same y-aches
             and in a distance of 3"""
         p1 = np.array([1, 3, 5]).reshape(3)
@@ -50,35 +51,28 @@ class TestUtil(unittest.TestCase):
         for i in range(len(assert_list)):
             self.assertEqual(assert_list[i], close_list[i])
 
-    def test_util_getPointsCloseToAnchor_Find_balcony(self):
-        """Test the method "get points close to anchor" withe a point on the roof and
-            some points of a balcony and a gutter."""
-        """Balconiy coord"""
-        """right side"""
+    def test_getPointsCloseToAnchor_Find_balcony(self):
+        """Test the method "get points close to anchor" withe an negativ integer as distance"""
+        """coords"""
         b1 = np.array([5, 8, 3]).reshape(3)
-        b2 = np.array([5.5, 8, 2.5]).reshape(3)
-        b3 = np.array([6, 8, 2]).reshape(3)
-        """front side"""
-        b4 = np.array([6.5, 8, 2]).reshape(3)
-        b5 = np.array([6.7, 8, 2.5]).reshape(3)
-        b6 = np.array([6.9, 8, 3]).reshape(3)
-        b7 = np.array([7, 8, 3.5]).reshape(3)
-        """left side"""
-        b8 = np.array([6, 8, 3.7]).reshape(3)
-        b9 = np.array([5.5, 8, 4]).reshape(3)
-        """gutter coord"""
-        g1 = np.array([5.5, 12, 3]).reshape(3)
-        g2 = np.array([5.5, 12, 4]).reshape(3)
+        b2 = np.array([1, 0, 3]).reshape(3)
+        b3 = np.array([6, 1, 2]).reshape(3)
+        b4 = np.array([1, 3, 2]).reshape(3)
+        b5 = np.array([5, 2, 5]).reshape(3)
+        """all coords"""
+        points = [b1, b2, b3, b4, b5]
+        numpy_arr = np.vstack(points)
         """anchor cord"""
         anchor = [5, 8, 4]
-        """all coords"""
-        points = [b1, b2, b3, b4, b5, b6, b7, b8, b9, g1, g2]
-        distance = 3
-        numpy_arr = np.vstack(points)
-        assert_list = [True, True, True, True, True, True, True, True, True, False, False]
+        distance = -3
         close_list = util.getPointsCloseToAnchor(anchor, numpy_arr, distance)
-        for i in range(len(assert_list)):
-            self.assertEqual(assert_list[i], close_list[i])
+        self.assertEqual(None, close_list)
+
+    def test_unzip_LAZFILE(self):
+        if os.path.exists("example_data/pointcloud_examples/47078_575411_0011.las"):
+            os.remove("example_data/pointcloud_examples/47078_575411_0011.las")
+        util.unzipLAZFile("example_data/pointcloud_examples/47078_575411_0011.laz")
+        self.assertTrue(os.path.exists("example_data/pointcloud_examples/47078_575411_0011.las"))
 
     def tearDown(self) -> None:
         pass
