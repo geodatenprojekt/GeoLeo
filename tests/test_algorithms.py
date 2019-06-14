4c4a5dc35caf7d04f4315f82b55881df2eab661d
valentin235 <valentin.hertel@fh-bielefeld.de>
2019-06-14 11:26:38 +0200

    change _x, _y to x and y

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -12,17 +12,17 @@ class TestAlgorithms (unittest.TestCase):
         """Test the method "shift cadaster coordinates" withe the first coordinate
             of the first building and the last coordinate od the last building.
         """
-        buildings = cadasta.get_buildings("../example_data/cadaster_examples")
-        firstx = buildings[0].coordinates[0]._x
-        lastx = buildings[len(buildings)-1].coordinates[len(buildings[len(buildings)-1].coordinates)-1]._x
-        firsty = buildings[0].coordinates[0]._y
-        lasty = buildings[len(buildings)-1].coordinates[len(buildings[len(buildings)-1].coordinates)-1]._y
+        buildings = cadasta.get_buildings("example_data/cadaster_examples")
+        firstx = buildings[0].coordinates[0].x
+        lastx = buildings[len(buildings)-1].coordinates[len(buildings[len(buildings)-1].coordinates)-1].x
+        firsty = buildings[0].coordinates[0].y
+        lasty = buildings[len(buildings)-1].coordinates[len(buildings[len(buildings)-1].coordinates)-1].y
         offset = [5, 10]
 
         algorithms.shiftCadasterCoordinates(buildings, offset)
-        self.assertEqual(firstx+offset[0], buildings[0].coordinates[0]._x)
+        self.assertEqual(firstx+offset[0], buildings[0].coordinates[0].x)
         self.assertEqual(lastx+offset[0], buildings[len(buildings)-1]
-                         .coordinates[len(buildings[len(buildings)-1].coordinates)-1]._x)
-        self.assertEqual(firsty+offset[1], buildings[0].coordinates[0]._y)
-        self.assertEqual(lasty+offset[0], buildings[len(buildings)-1]
-                         .coordinates[len(buildings[len(buildings)-1].coordinates)-1]._y)
+                         .coordinates[len(buildings[len(buildings)-1].coordinates)-1].x)
+        self.assertEqual(firsty+offset[1], buildings[0].coordinates[0].y)
+        self.assertEqual(lasty+offset[1], buildings[len(buildings)-1]
+                         .coordinates[len(buildings[len(buildings)-1].coordinates)-1].y)
