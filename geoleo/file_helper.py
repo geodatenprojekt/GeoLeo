078d0ba25044b65ad448cf67b1c64c7110362519
trinkmuetze <michelle.vorwerk@fh-bielefeld.de>
2019-06-19 11:34:15 +0200

    you can now get all files with a specific file ending

--- geoleo/file_helper.py
+++ geoleo/file_helper.py
@@ -4,15 +4,19 @@
 from os import listdir
 from os.path import isfile, join
 
-def get_all_paths_from_dir(path_to_dir):
+def get_all_paths_from_dir(path_to_dir, file_ending=None):
     """Returns a list with all file paths in a given directory path
 
     Keyword arguments:
     path_to_dir -- the path to the directoy, which do you want all file paths
     """
+
     list_file_paths = []
     for file_name in listdir(path_to_dir):
-        file_path = join(path_to_dir, file_name)
+        file_path = ""
+        if file_ending is not None:
+            if file_name.endswith(file_ending):
+                file_path = join(path_to_dir, file_name)
         if isfile(file_path):
             list_file_paths.append(file_path)
     return list_file_paths
